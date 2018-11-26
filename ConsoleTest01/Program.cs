e6961de0305598d483c3105d547357ee8aca1298
yoyoxps7 <yoyoxps7@YOYOXPSD7>
2018-11-26 00:12:13 -0500

    Add .gitignore and .gitattributes.

---
+++ ConsoleTest01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleTest01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Test");
+        }
+    }
+}
